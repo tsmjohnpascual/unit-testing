64d5968888d7229f09359bdddf9af055b14ea558
johnpascual <ashleypascual@hotmail.es>
2022-07-12 11:03:57 +0100

    Refactor tests to use each

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -5,9 +5,41 @@ describe("getLargest", () => {
         expect(typeof getLargest).toBe("function");
     })
     
-    test("It returns the largest value", () => {
-        const result = getLargest(1, 2, 3);
-        expect(result).toEqual(3);
+    // test("It returns the largest value", () => {
+    //     const result = getLargest(1, 2, 3);
+    //     expect(result).toEqual(3);
+    // })
+
+    describe("It handles normal inputs successfully", () => {
+
+        // test("", () => {
+        //     const result = getLargest(1, 2, 3)
+        //     expect(result).toEqual(3)
+        // })
+
+        // test("", () => {
+        //     const result = getLargest(3, 3, 3)
+        //     expect(result).toEqual(3)
+        // })
+
+        // test("", () => {
+        //     const result = getLargest(-1, -2, 3)
+        //     expect(result).toEqual(3)
+        // })
+
+        // test("", () => {
+        //     const result = getLargest(0.4, 1.5, 3)
+        //     expect(result).toEqual(3)
+        // })
+
+        each([
+            [[1, 2, 3], 3],
+            [[9, 8, 7], 9],
+            [[12, 36, 19], 36],
+        ]).test("", (arr, expected) => {
+            expect(getLargest(arr).toEqual(expected))
+        })
+
     })
 })
 
